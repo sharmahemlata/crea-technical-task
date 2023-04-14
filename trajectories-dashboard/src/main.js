7d017b013776074fc942bd1481ce6bcf5085ab99
Hemlata Sharma <hemlata.sharma@koneksys.com>
2023-04-14 18:37:03 +0530

    update

--- trajectories-dashboard/src/main.js
+++ trajectories-dashboard/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+new Vue({
+  render: (h) => h(App)
+}).$mount('#app')
